9c535b2ac3e791bacc9b8666111fe823beb83dd4
JJ Kasper <jj@jjsweb.site>
2020-04-16 09:11:46 -0500

    Add AMP step

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,12 +1,26 @@
 import Link from 'next/link'
+import { PageConfig } from 'next'
+
+export const config: PageConfig = {
+  amp: true
+}
 
 export default () => {
   return (
     <>
       <p>About Page</p>
-      <Link href='/'>
-        <a>To /</a>
-      </Link>
+      <div>
+        <Link href='/'>
+          <a>To /</a>
+        </Link>
+      </div>
+
+      <amp-img
+        src="https://unsplash.it/800/300?id=123"
+        alt="demo image"
+        height="300"
+        width="500"
+      />
     </>
   )
 }
